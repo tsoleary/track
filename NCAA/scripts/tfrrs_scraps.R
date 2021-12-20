00cd6cdfe72b41cff0d5c6be234d661a6714241a
tsoleary <tsoleary12@gmail.com>
2021-12-20 16:47:49 -0500

    update scraps

--- NCAA/scripts/tfrrs_scraps.R
+++ NCAA/scripts/tfrrs_scraps.R
@@ -8,53 +8,36 @@
 library(tidyverse)
 
 # Load data
-df <- readRDS(here::here("NCAA/data/tfrrs_NCAA_2010_2021_df.rds"))
+df <- readRDS(here::here("NCAA/data/tfrrs_NCAA_2010_2021_final_data.rds"))
 
-# Fastest in each event each year 
-df_tops <- df %>%
-  group_by(Event, Year) %>%
-  slice_max(1)
+# Denote Track, Field, & Multi Events
+df <- df %>%
+  mutate(EVENT_TYPE = case_when(!is.na(TIME_S) ~ "TRACK",
+                                !is.na(MARK_METERS) ~ "FIELD",
+                                !is.na(POINTS) ~ "MULTI"))
+
+
+x <- df %>%
+  filter(EVENT_TYPE == "TRACK") %>%
+  group_by(EVENT, SEX) %>%
+  group_split()
+
+for (i in 1:length(x)) {
+  p <- x[[i]] %>%
+    ggplot(aes(x = YEAR_QUAL, y = TIME_M)) +
+    geom_violin(fill = "azure2", color = "grey40") +
+    geom_boxplot(fill = "azure2", color = "grey40", width = 0.2) +
+    labs(title = paste0(unique(x[[i]]$SEX), "'s ", unique(x[[i]]$EVENT))) +
+    theme_classic()
+  
+  print(p)
+}
 
 
 df %>%
-  filter(Event == "200 Meters (Men)") %>%
-  mutate(time = as.numeric(str_remove_all(TIME, "@"))) %>%
-  ggplot() +
-  geom_histogram(aes(x = time), 
-                 bins = 30,
-                 color = "grey50", 
-                 fill = "grey70") +
-  labs(y = "Count",
-       x = "Time") +
-  theme_classic() + 
-  facet_wrap( ~ Year)
-
-
-df %>%
-  filter(Event == "100 Meters (Men)") %>%
-  mutate(time = as.numeric(str_remove_all(TIME, "@"))) %>%
-  ggplot(aes(y = time, x = Year)) +
-  geom_violin(fill = "azure2") +
-  geom_boxplot(color = "grey50", fill = "azure", width = 0.2) +
-  #geom_hline(yintercept = 19.5, color = "grey10", linetype = "dashed") + 
-  #geom_smooth(aes(y = time, x = as.numeric(Year)), method = lm) +
-  labs(y = "Time",
-       x = "Year",
-       title = "100 Meters (Men)") +
+  filter(EVENT == "Long Jump" & SEX == "Men") %>%
+  ggplot(aes(x = YEAR_QUAL, y = MARK_METERS)) +
+  geom_violin(fill = "azure2", color = "grey40") +
+  geom_boxplot(fill = "azure2", color = "grey40", width = 0.2) +
   theme_classic()
 
-y <- df %>%
-  filter(Event == "1500 Meters (Men)") %>%
-  mutate(time = str_remove_all(TIME, "@")) %>%
-  separate(time, into = c("mins", "secs"), sep = ":") %>%
-  mutate(mins = as.numeric(mins),
-         secs = as.numeric(secs)) %>%
-  mutate(time_s = ((mins * 60) + secs)) %>%
-  ggplot(aes(y = time_s, x = Year)) +
-  geom_violin(fill = "azure2") +
-  geom_boxplot(color = "grey50", fill = "azure", width = 0.2) +
-  # geom_hline(yintercept = 19.5, color = "grey10", linetype = "dashed") +
-  # geom_smooth(aes(y = time, x = as.numeric(Year)), method = lm) +
-  labs(y = "Time",
-       x = "Year") +
-  theme_classic()
